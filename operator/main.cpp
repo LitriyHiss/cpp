b477b53c7e085b53762f9953d7e062861882c7d5
LitriyHiss <147576319+LitriyHiss@users.noreply.github.com>
2024-10-03 01:21:45 +0700

    Update main.cpp

--- operator/main.cpp
+++ operator/main.cpp
@@ -20,13 +20,6 @@ public:
         }
         return data[index];
     }
-
-    const T& operator[](int index) const {
-        if (index < 0 || index >= size) {
-            throw std::out_of_range("Index out of range");
-        }
-        return data[index];
-    }
 };
 
 int main() {
